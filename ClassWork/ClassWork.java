2c63a15545950c04a3ecf35e4fd2aecb6c1d2cf1
Pedro Henrique D'Oliveira <phd19994@gmail.com>
2026-05-06 22:42:04 -0300

    feature: player - grid - validation methods

--- ClassWork/ClassWork.java
+++ ClassWork/ClassWork.java
@@ -2,6 +2,68 @@
 // to run with less boilerplate code use command
 // $ java --enable-preview folder/file.java
 
-void main(){
-    System.out.println("works");
+import java.util.Arrays;
+
+void main(){ // simplified version for JDK 21+
+System.out.println("Main Class Running...");
+TicTacToe game = new TicTacToe();
+// game.ShowGameBoard();
+game.player1Move(0, 0);
+game.player2Move(0, 1);
+game.player1Move(0, 0);
+game.showGameBoard();
+
+}
+
+public class TicTacToe {
+
+    // global variables
+    // static methods exists before objects are created
+
+    String[][] boardGrid = {
+            { " ", " ", " "},
+            { " ", " ", " "},
+            { " ", " ", " "}
+            // { null, null, null}
+            // { null, null, null}
+            // { null, null, null}
+    };
+
+    public boolean checkMove(int r, int c){
+        String grid = boardGrid[r][c];
+
+        if (grid == " ") {
+            System.out.println(" CAN PLAY MOVE ");
+            return true;
+        };
+
+        System.out.println(" CAN NOT PLAY MOVE ");
+        return false;
+
+    }
+
+    public void player1Move(int r, int c){
+        // define player 1 with X
+        if (checkMove(r, c)) {
+            boardGrid[r][c] = "X";
+        }
+        
+
+    }
+
+    public void player2Move(int r, int c) {
+        // define player 2 with O
+        if (checkMove(r, c)) {
+            boardGrid[r][c] = "O";
+        };
+        
+    }
+
+    public void showGameBoard() {
+        // System.out.println(Arrays.deepToString(boardGrid));
+        System.out.println(boardGrid[0][0] + " | " + boardGrid[0][1] + " | " + boardGrid[0][2]);
+        System.out.println(boardGrid[1][0] + " | " + boardGrid[1][1] + " | " + boardGrid[1][2]);
+        System.out.println(boardGrid[2][0] + " | " + boardGrid[2][1] + " | " + boardGrid[2][2]);
+    }
+
 }
