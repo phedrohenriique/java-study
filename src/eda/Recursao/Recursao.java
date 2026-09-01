c94bd5ba813b567c372dd5a35e4c4877c05a13b6
Pedro Henrique <phd19994@gmail.com>
2026-09-01 20:10:49 -0300

    feature: recursion

--- src/eda/Recursao/Recursao.java
+++ src/eda/Recursao/Recursao.java
@@ -4,6 +4,57 @@ public class Recursao {
 
     public static void main(String[] args) {
 
-    }; // simplified version for JDK 21+
+        // CalculaMDC(48, 56);
+
+        // int result = Fatorial(5); // result == 120
+        // System.out.println(result);
+
+        // String result = decimalToBinary(10);
+        // System.out.println(result);
+
+        int result = sumDigits(1111);
+        System.out.println(result);
+
+    }; 
+
+    public static int CalculaMDC(int m, int n) {
+
+        if (n == 0) {
+            System.out.println(m);
+            return m;
+        }
+
+        
+        return CalculaMDC(n, m % n);
+    }
+
+    public static int Fatorial(int x){
+
+        if(x == 0){
+            
+            return 1;
+        }
+
+        return x * Fatorial(x - 1);
+
+    }
+
+    public static String decimalToBinary(int n) {
+
+        if (n < 2) {
+            return String.valueOf(n);
+        }
+    
+        return decimalToBinary(n / 2) + (n % 2);
+    }
+
+    public static int sumDigits(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+    
+        return (n % 10) + sumDigits(n / 10);
+    }
 
 }
